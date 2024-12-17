ba4e16c4840c321bce138de6d3811ed8d7bbbf96
AbdulWahabXVI <113459776+AbdulWahabXVI@users.noreply.github.com>
2024-12-17 12:16:47 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const sidebarToggle = document.querySelector("#sidebar-toggle");
+sidebarToggle.addEventListener("click",function(){
+    document.querySelector("#sidebar").classList.toggle("collapsed");
+});
+
+document.querySelector(".theme-toggle").addEventListener("click",() => {
+    toggleLocalStorage();
+    toggleRootClass();
+});
+
+function toggleRootClass(){
+    const current = document.documentElement.getAttribute('data-bs-theme');
+    const inverted = current == 'dark' ? 'light' : 'dark';
+    document.documentElement.setAttribute('data-bs-theme',inverted);
+    if(current = 'dark'){
+        document.getElementByClassName('theme-toggle').style.color = 'black';
+    }
+    if(current = 'light'){
+        document.getElementByClassName('theme-toggle').style.color = 'white';
+    }
+}
+
+function toggleLocalStorage(){
+    if(isLight()){
+        localStorage.removeItem("light");
+    }else{
+        localStorage.setItem("light","set");
+    }
+}
+
+function isLight(){
+    return localStorage.getItem("light");
+}
+
+if(isLight()){
+    toggleRootClass();
+}
